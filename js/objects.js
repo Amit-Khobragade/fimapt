9edde7c50e3821b154eb6334506205cd2f721521
Amit Khobragade <amitkh1102@outlook.com>
2021-11-15 21:23:18 +0530

    Added new function constructors to help with oop The function super constructor named activity is made to ensure that any new activities could easily be added to the page. Also two default activities namely Walking and Cycling has been introduced which are children constructors to Activities. Also a method named get prompt had been added to the activity prototype to get the div automatically constructor further implementing encapsulation.

---
+++ js/objects.js
@@ -0,0 +1,56 @@
+function Activity(date, distance, duration, marker, type) {
+	this.date = date;
+	this.distance = distance;
+	this.duration = duration;
+	this.type = type;
+	this.marker = marker;
+
+	this.speed = (distance * 1000) / (duration * 60);
+	this.speed = Math.round(this.speed * 100) / 100;
+
+	this.prompt = document.createElement('div');
+	this.prompt.classList.add('task');
+	this.prompt.dataset.type = this.type;
+	this.prompt.innerHTML = `
+    <h2 class="verbose">
+      <span class="type"></span>
+      on
+      <span class="date"></span>
+    </h2>
+    <div class="details">
+      <span class="dist"></span>
+      <span class="time"></span>
+      <span class="speed"></span>
+      <span class="spec"></span>
+    </div>
+  `;
+	this.prompt.querySelector('.type').innerText = this.type;
+	this.prompt.querySelector('.date').innerText = this.date;
+	this.prompt.querySelector('.dist').innerText = `${distance} KM`;
+	this.prompt.querySelector('.time').innerText = `⌚${duration} min`;
+	this.prompt.querySelector('.speed').innerText = `⚡${this.speed} m/s`;
+}
+
+Activity.prototype.getPrompt = function () {
+	return this.prompt;
+};
+
+function Walking(date, distance, duration, marker, spm) {
+	Activity.call(this, date, distance, duration, marker, 'walking');
+	this.prompt.querySelector('.spec').innerText = `👟 ${spm} spm`;
+	this.prompt.querySelector('.dist').innerText =
+		'🏃' + this.prompt.querySelector('.dist').innerText;
+}
+
+Walking.prototype = Object.create(Activity.prototype);
+Walking.prototype.constructor = Walking;
+
+function Cycling(date, distance, duration, marker, elev) {
+	Activity.call(this, date, distance, duration, marker, 'cycling');
+	this.prompt.querySelector('.spec').innerText = `🗻 ${elev} meters`;
+	this.prompt.querySelector('.dist').innerText =
+		'🚴' + this.prompt.querySelector('.dist').innerText;
+}
+
+Cycling.prototype = Object.create(Activity.prototype);
+Cycling.prototype.constructor = Cycling;
